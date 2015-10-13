d45c30aed17d56863995b1d2343c2775dc7ec0f7
juan pablo <juan pablo@juanpablo-PC>
2015-10-13 10:13:39 -0300

    Aplicación métodos 2

--- RectaMetodos/src/rectametodos/RectaMetodos.java
+++ RectaMetodos/src/rectametodos/RectaMetodos.java
@@ -5,74 +5,96 @@
  */
 package rectametodos;
 import java.util.*;
+import java.text.DecimalFormat;
 /**
  *
  * @author juan pablo
  */
 public class RectaMetodos {
-    public static double leer(){
-        String x;
-        Scanner leer=new Scanner(System.in);
-        x=leer.next();
-        return Double.parseDouble(x);
-    }
-    
-    public static void imprimir(String i){
-        System.out.print(i);
-    }
-    
-    public static double pendiente(double x1, double y1 , double x2, double y2){
-        
-       return (y2-y1)/(x2-x1);
-       
-    
-    }
-    
-    public static double interseccion(double x1, double y1, double m){
-       
-        return y1-m*x1;
-        
-    
-   }
     
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double x1,y1,x2,y2,m,b;
+        double x1,y1,x2,y2;
         
         try{
-        System.out.print("Ingrese x1");
-        x1=leer(); 
-        System.out.print("Ingrese y1");
-        y1=leer();
-       
-        System.out.print("Ingrese x2");
-        x2=leer();
-        
-        System.out.print("Ingrese y2");
-        y2=leer();
-       
+             x1=ingresarPuntoX1();
+             y1=ingresarPuntoY1();
+             x2=ingresarPuntoX2();
+             y2=ingresarPuntoY2();
       
-       if(x2==x1)
-            imprimir("Error");
-       if(x2!=x1){
+            if(x2==x1){
+                imprimir("ERROR: x1 es igual a x2");
+                System.out.println();
+                RectaMetodos.main(args);
+            }
+                
+            if(x2!=x1){
+                ecuacion(x1,y1,x2,y2);
+            }
        
-       
-        imprimir("La Ecuacion de la recta es: Y="+ pendiente(x1,y1,x2,y2) + "X + "+interseccion(x1,y1,pendiente(x1,y1,x2,y2)) );
-        }
-        }
+            }
         catch(Exception e){
-          
-       
-         imprimir("ERROR");
-        
-        
+            
+            imprimir("ERROR: Ingrese un número");
+            System.out.println();
+            RectaMetodos.main(args);
+            
+            
         }
     
         
         // TODO code application logic here
     }
+    public static void imprimir(String i){
+        System.out.print(i);
+    }
     
+    public static double pendiente(double x1, double y1 , double x2, double y2){
+        return (y2-y1)/(x2-x1);
+    }
+    
+    public static double interseccion(double x1, double y1, double m){
+       return y1-m*x1;
+    }
+    
+    public static double ingresarPuntoX1(){
+       String p;
+       System.out.print("ingrese x1: ");
+       Scanner leer= new Scanner(System.in);
+       p=leer.next();
+       return Double.parseDouble(p);      
+    }
+    
+    public static double ingresarPuntoY1(){
+       String p;
+       System.out.print("ingrese y1: ");
+       Scanner leer= new Scanner(System.in);
+       p=leer.next();
+       return Double.parseDouble(p);
+    }
+    
+    public static double ingresarPuntoX2(){
+       String p;
+       System.out.print("ingrese x2: ");
+       Scanner leer= new Scanner(System.in);
+       p=leer.next();
+       return Double.parseDouble(p);
+    }
+    public static double ingresarPuntoY2(){
+       String p;
+       System.out.print("ingrese y2: ");
+       Scanner leer= new Scanner(System.in);
+       p=leer.next();
+       return Double.parseDouble(p);
+    }
+    public static void ecuacion(double x1, double y1, double x2, double y2){
+       DecimalFormat df = new DecimalFormat("0.0");
+       System.out.print("La Ecuacion de la recta es: Y="+ df.format(pendiente(x1,y1,x2,y2))
+               +"x+ "+df.format(interseccion(x1,y1,pendiente(x1,y1,x2,y2))));
+       
+   }
+   
 }
